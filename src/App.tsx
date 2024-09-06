339afa231001f5d20e0631507c763df49da83a1d
Nicolas Wormser <nicolas@codewithflair.org>
2024-09-06 13:01:27 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,30 @@ import { useState } from "react"
 type Coordinates = [0 | 1 | 2, 0 | 1 | 2]
 
 const WINNING_CASES = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [2, 2, 2],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [2 ,1, 0],
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
+  [[0, 0], [0,1], [0,2]]
 ]
 
 function App() {
   const [turn, setTurn] = useState(true)
   // fixme - choose a tableau de coords
-  const [crosses, setCrosses] = useState<Array<number[]>>([[], [], []])
-  const [circles, setCircles] = useState<Array<number[]>>([[], [], []])
+  const [crosses, setCrosses] = useState<Array<Coordinates>>([])
+  const [circles, setCircles] = useState<Array<Coordinates>>([])
   const [winner, setWinner] = useState(null)
 
-  const handleClick = (x: Coordinates[0], y: Coordinates[1]) => {
+  const handleClick = (selectedPosition: Coordinates) => {
     ;(turn ? setCrosses : setCircles)((old) =>
-      old.map((column, i) => (i === x ? [...column, y] : column))
+      [...old, selectedPosition]
     )
     determineWinner()
     setTurn(!turn)
@@ -39,13 +39,14 @@ function App() {
 
         //[0, 0, 0] =>
         //[2 ,1, 0]
-            comb.every((position: number, k: number) => player[k].includes(position))
+            comb.every((position: Coordinates, k: number) => player[k].includes(position))
         }
       })
-    })
 
-  const fillCell = (x: Coordinates[0], y: Coordinates[1]) =>
-    (crosses[x].includes(y) && "X") || (circles[x].includes(y) && "O")
+
+  const fillCell = (target: Coordinates) =>
+    (crosses.some(it => it[0]===target[0] && it[1]===target[1]) && "X") 
+  || (circles.some(it => it[0]===target[0] && it[1]===target[1]) && "O")
 
   return (
     <div data-test="grid" className="container">
@@ -54,7 +55,7 @@ function App() {
         return (
           <div
             data-test={`cell-${x}-${y}`}
-            onClick={handleClick.bind(null, x, y)}
+            onClick={handleClick.bind(null, [x, y])}
           >
             {fillCell(x, y)}
           </div>
